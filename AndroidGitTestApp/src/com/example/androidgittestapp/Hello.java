a802e8a957f1480d271d45df4322adedf011d5d7
kolithas <kolithas@users.noreply.github.com>
2015-01-06 12:25:37 +0530

    Commit - 4
    Signed-off-by: kolithas < kolithas@users.noreply.github.com>

--- AndroidGitTestApp/src/com/example/androidgittestapp/Hello.java
+++ AndroidGitTestApp/src/com/example/androidgittestapp/Hello.java
@@ -2,6 +2,7 @@ package com.example.androidgittestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Hello extends Activity {
 
@@ -9,5 +10,11 @@ public class Hello extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onContextMenuClosed(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onContextMenuClosed(menu);
 	}
 }
