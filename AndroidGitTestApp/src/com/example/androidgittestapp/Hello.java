753f75d479b8f4e7e83da4702b25c26532e5ea8d
kolithas <kolithas@users.noreply.github.com>
2015-01-06 12:17:32 +0530

    commit-03
    Signed-off-by: kolithas < kolithas@users.noreply.github.com>

--- AndroidGitTestApp/src/com/example/androidgittestapp/Hello.java
+++ AndroidGitTestApp/src/com/example/androidgittestapp/Hello.java
@@ -1,7 +1,13 @@
 package com.example.androidgittestapp;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Hello extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
